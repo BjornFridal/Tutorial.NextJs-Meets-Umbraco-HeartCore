44c660dc6ca26fb820958222e7d77fda7362785e
Bjørn Fridal <bjorn@fridal.dk>
2021-02-08 10:16:18 +0100

    Remove leftover from framer-motion

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <div className="relative overflow-hidden bg-yellow-100">
       <div className="flex h-screen -mr-0.5">
-        <BandMember name="Joey" url="/members/joey" slideInFromTop={true} />
-        <BandMember name="Johnny" url="/members/joey" slideInFromTop={false} />
-        <BandMember name="Dee Dee" url="/members/joey" slideInFromTop={true} />
-        <BandMember name="Tommy" url="/members/joey" slideInFromTop={false} />
+        <BandMember name="Joey" url="/members/joey" />
+        <BandMember name="Johnny" url="/members/joey" />
+        <BandMember name="Dee Dee" url="/members/joey" />
+        <BandMember name="Tommy" url="/members/joey" />
       </div>
 
       <Footer htmlTag="h1" />
@@ -16,7 +16,7 @@ export default function Home() {
   );
 }
 
-const BandMember = ({ name, url, slideInFromTop }) => (
+const BandMember = ({ name, url }) => (
   <div className="flex flex-1 justify-center items-center text-3xl font-bold bg-yellow-200 mr-0.5 text-center hover:bg-yellow-100">
     <Link href={url}>{name}</Link>
   </div>
